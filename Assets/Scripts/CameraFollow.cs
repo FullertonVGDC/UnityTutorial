95697048ac843dfb8d671d9838c6416aac18f05d
Kien Ngoc Nguyen <tgsoon2002@gmail.com>
2017-10-06 15:16:35 -0700

    First tutorial
    - Set up canvas for background
    - Add sprite sheet and slice it
    - add script to move and jupm
    - add environment object and iteract with it

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform character;
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(character.position.x, character.position.y,0);
+	}
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	#region data member
+	Rigidbody2D rigid;
+	public	Sprite left;
+	public	Sprite right;
+	SpriteRenderer render;
+	#endregion
+
+	#region Build-in Method
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody2D> ();
+		render = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// move base on the horizontal left and right arrow
+		rigid.AddForce(new Vector2(Input.GetAxis("Horizontal") * 20,0));
+		// update the spite to look like
+		if (Input.GetAxis ("Horizontal") > 0.2f) {
+			render.sprite = left;
+		} else if (Input.GetAxis ("Horizontal") < -0.2f)  {
+			render.sprite = right;
+				}
+		// Jump if player press Ctrl or Left mouse
+		if (Input.GetButtonDown("Fire1")) {
+			rigid.AddForce(new Vector2(0,120));	
+				}
+	}
+	#endregion
+}
